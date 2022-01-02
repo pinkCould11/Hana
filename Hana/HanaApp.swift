d7839447325f3c5fa4eb259b608f513203f32ee4
Hana <jlucy0411@gmail.com>
2022-01-01 18:23:22 -0600

    Initial Commit

---
+++ Hana/HanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanaApp.swift
+//  Hana
+//
+//  Created by Hana Keinan on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
